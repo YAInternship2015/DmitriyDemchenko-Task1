3a3401fc37520fe2c1aa1e9f10a9ad0b6aff392b
Dmitriy Demchenko <organization_98@yahoo.com>
2015-08-29 15:18:05 +0300

    Refactor: DDContainerSegue class added

---
+++ Cartoons/Cartoons/Cells/DDContainerSegue/DDContainerSegue.h
@@ -0,0 +1,13 @@
+//
+//  DDContainerSegue.h
+//  Cartoons
+//
+//  Created by Dmitriy Demchenko on 8/29/15.
+//  Copyright (c) 2015 Dmitriy Demchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDContainerSegue : UIStoryboardSegue
+
+@end
