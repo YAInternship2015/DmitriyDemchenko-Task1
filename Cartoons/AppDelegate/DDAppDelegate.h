ed59e1950762107dbde15c86e546e1d5665778a8
Dmitriy Demchenko <organization_98@yahoo.com>
2015-09-15 23:58:00 +0300

    .gitignore fixed for CocoaPods

--- Cartoons/AppDelegate/DDAppDelegate.h
+++ Cartoons/AppDelegate/DDAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-#warning я забыл упомянуть один момент в описании задания. При использовании cocoapods в .gitignore нужно добавить папку Pods, она не должна лежать в гите
+//#warning я забыл упомянуть один момент в описании задания. При использовании cocoapods в .gitignore нужно добавить папку Pods, она не должна лежать в гите
 
 @interface DDAppDelegate : UIResponder <UIApplicationDelegate>
 
